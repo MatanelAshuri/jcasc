83f00400071dbb102b645e75529a91f7778ebba9
MatanelAshuri <Matanelashuri@gmail.com>
2023-01-30 14:36:42 +0200

    Update jenkinsfile2.groovy
    r

--- jenkinsfile2.groovy
+++ jenkinsfile2.groovy
@@ -1,17 +1,29 @@
 pipeline {
     agent any
     stages {
-        stage("Create directory, init tf, start instance and show key and details") {
-            steps {
+        stage("Create directory") {
+            steps {               
                 script {
-                    sh "mkdir /var/n_instances/${BUILD_NUMBER}"     
+                    sh "mkdir /var/n_instances/${BUILD_NUMBER}" 
                     sh "cp /var/n_instances/highest /var/n_instances/${BUILD_NUMBER}"
-                    sh "cp /var/n_instances/p2tf.tf /var/n_instances/${BUILD_NUMBER}"
-                    sh "cd /var/n_instances/${BUILD_NUMBER} && terraform init && terraform plan"
-                    sh "terraform apply -auto-approve"
-                    archiveArtifacts artifacts: '/var/n_instances/${BUILD_NUMBER}/p2m-key'
+                    sh "cp /var/n_instances/p2tf.tf /var/n_instances/${BUILD_NUMBER}"					
                 }
             }
         }
+		stage("Create directory, init tf, start instance and show key and details") {
+            steps {
+                dir("/var/n_instances/${BUILD_NUMBER}") { 
+                    sh 'terraform init'
+					}
+				}
+            }
+		stage("Do the needful") {
+            steps {               
+                dir("/var/n_instances/${BUILD_NUMBER}") { 
+                    sh "cd /var/n_instances/${BUILD_NUMBER} && terraform init && terraform plan"
+                    sh "terraform apply -auto-approve"
+                }
+            }
+          }            
+        }
     }
-}
