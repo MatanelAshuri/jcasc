342be133c8567d0a5742f3e2f7d7f3459d1c2d30
MatanelAshuri <Matanelashuri@gmail.com>
2023-01-30 13:24:32 +0200

    Update jenkinsfile2.groovy
    fff

--- jenkinsfile2.groovy
+++ jenkinsfile2.groovy
@@ -5,7 +5,7 @@ pipeline {
             steps {
                 script {
                     sh "mkdir /var/n_instances/${BUILD_NUMBER}"                    
-                    sh "cp /var/instances/p2tf.tf /var/n_instances/${BUILD_NUMBER}"
+                    sh "cp /var/n_instances/p2tf.tf /var/n_instances/${BUILD_NUMBER}"
                     sh "cd /var/n_instances/${BUILD_NUMBER} && terraform init"
                     sh "terraform apply -auto-approve"
                     archiveArtifacts artifacts: '/var/n_instances/${BUILD_NUMBER}/p2m-key'
