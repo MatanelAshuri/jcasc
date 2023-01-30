abb769a3ad92de3defb8f082d2d4569d9a3d4b57
MatanelAshuri <Matanelashuri@gmail.com>
2023-01-30 13:34:39 +0200

    Update jenkinsfile2.groovy
    fffff

--- jenkinsfile2.groovy
+++ jenkinsfile2.groovy
@@ -4,7 +4,8 @@ pipeline {
         stage("Create directory, init tf, start instance and show key and details") {
             steps {
                 script {
-                    sh "mkdir /var/n_instances/${BUILD_NUMBER}"                    
+                    sh "mkdir /var/n_instances/${BUILD_NUMBER}"     
+                    h "cp /var/n_instances/highest /var/n_instances/${BUILD_NUMBER}"
                     sh "cp /var/n_instances/p2tf.tf /var/n_instances/${BUILD_NUMBER}"
                     sh "cd /var/n_instances/${BUILD_NUMBER} && terraform init"
                     sh "terraform apply -auto-approve"
